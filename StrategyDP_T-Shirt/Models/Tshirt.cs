eca54fd6add3b8b6190169cb8012eee8b6af56b0
Cr1m3b <iosifvasoul@yahoo.gr>
2022-02-28 20:32:08 +0100

    Model Tshirt Init

---
+++ StrategyDP_T-Shirt/Models/Tshirt.cs
@@ -0,0 +1,24 @@
+﻿using StrategyDP_T_Shirt.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDP_T_Shirt.Models
+{
+    public class Tshirt
+    {
+        public Color Color;
+        public Fabric Fabric;
+        public Size Size;
+        public decimal Price { get; set; }
+        public Tshirt(Color color, Fabric fabric, Size size)
+        {
+            Color = color;
+            Fabric = fabric;
+            Size = size;
+        }
+
+    }
+}
